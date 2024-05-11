b5d2d51c5dea27084371c22b2358b1d1206f8b9b
Rod <snakepliskin30@gmail.com>
2024-05-12 01:19:43 +0800

    setup mongodb in the backend

--- api/index.js
+++ api/index.js
@@ -1,4 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// IF IN CASE YOU CANNOT CONNECT TO THE DB
+// PLEASE MONGODB ATLAS PROJECT AND MAKE SURE THE IP IS UPDATED
+// UNDER NETWORK ACCESS. PLDT HAS DYNAMIC IP
+mongoose
+  .connect(
+    'mongodb+srv://snakepliskin30:snakepliskinpassword@mern-estate.yoxbrfy.mongodb.net/?retryWrites=true&w=majority&appName=mern-estate',
+  )
+  .then(() => {
+    console.log('connected to the db!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
