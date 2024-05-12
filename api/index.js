3f6b9e5264960388642de983ed395b68168c23e1
Rod <snakepliskin30@gmail.com>
2024-05-12 14:31:25 +0800

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,15 +2,15 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+
 dotenv.config();
 
 // IF IN CASE YOU CANNOT CONNECT TO THE DB
 // PLEASE MONGODB ATLAS PROJECT AND MAKE SURE THE IP IS UPDATED
 // UNDER NETWORK ACCESS. PLDT HAS DYNAMIC IP
 mongoose
-  .connect(
-    'mongodb+srv://snakepliskin30:snakepliskinpassword@mern-estate.yoxbrfy.mongodb.net/?retryWrites=true&w=majority&appName=mern-estate',
-  )
+  .connect(process.env.MONGODB)
   .then(() => {
     console.log('connected to the db!');
   })
@@ -24,4 +24,8 @@ app.listen(3000, () => {
   console.log('Server listening on port 3000!');
 });
 
+//this middleware will allow express to accept json in body
+app.use(express.json());
+
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
