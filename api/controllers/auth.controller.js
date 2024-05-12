3d08984fcef43512c738bf97d787ce022ba02f72
Rod <snakepliskin30@gmail.com>
2024-05-12 17:06:10 +0800

    added sign up functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,7 +2,9 @@ import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
 
 export const signup = async (req, res, next) => {
+  // await new Promise((resolve) => setTimeout(resolve, 2000));
   const { username, email, password } = req.body;
+  console.log(username, email, password);
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
   try {
